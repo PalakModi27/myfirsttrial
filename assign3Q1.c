788e94d32dd78e35c65a3e27a0819323f7966eb2
PalakModi27 <77453833+PalakModi27@users.noreply.github.com>
2021-01-14 23:25:32 +0530

    Add files via upload

---
+++ assign3Q1.c
@@ -0,0 +1,17 @@
+//WAP for any character entered through keyboard,to check whether it is capital,small letter,digit or special symbol
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("Enter the character: ");
+	scanf("%c",&ch);
+	if(ch>='A'&&ch<='Z')
+	printf("The entered character is a capital letter");
+	else if(ch>='a'&&ch<='z')
+	printf("The entered character is a small letter");
+	else if(ch>='0'&&ch<='9')
+	printf("The entered character is a digit");
+	else
+	printf("The entered character is a special symbol");
+	return 0;
+}
